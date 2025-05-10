511cbe4ca86377b9ed1e28f81784181667852c3c
Ridwan Ramdhani <gtiridwan@gmail.com>
2025-05-10 21:24:44 +0700

    Edit MataKuliah.java

--- src/main/java/com/atomic/getTentor/model/MataKuliah.java
+++ src/main/java/com/atomic/getTentor/model/MataKuliah.java
@@ -1,11 +1,13 @@
 package com.atomic.getTentor.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 
 @Entity
 @Table(name = "matakuliah")
 public class MataKuliah {
+    @Id
+    
+    @Column(length = 12, columnDefinition = "VARCHAR(12)")
     private String nama;
 
     public MataKuliah(String nama) {
