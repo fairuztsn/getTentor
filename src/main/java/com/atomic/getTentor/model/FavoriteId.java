f49ab6546982a15d9a39ea7c97ffb62282fcaa80
Gkyoto <dickarksr0725@gmail.com>
2025-05-21 17:50:59 +0700

    menambahkan FavoriteID
    wallahi im cooked

---
+++ src/main/java/com/atomic/getTentor/model/FavoriteId.java
@@ -0,0 +1,29 @@
+package com.example.favorite.model;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+public class FavoriteId implements Serializable {
+    private Integer mentee;
+    private Integer tentor;
+
+    public FavoriteId() {}
+
+    public FavoriteId(Integer mentee, Integer tentor) {
+        this.mentee = mentee;
+        this.tentor = tentor;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        FavoriteId that = (FavoriteId) o;
+        return Objects.equals(mentee, that.mentee) && Objects.equals(tentor, that.tentor);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(mentee, tentor);
+    }
+}
