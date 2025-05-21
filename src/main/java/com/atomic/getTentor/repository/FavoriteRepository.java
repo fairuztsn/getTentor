54a76aef071f84890bc4152803640d605e13f565
Gkyoto <dickarksr0725@gmail.com>
2025-05-21 19:03:34 +0700

    Adjusment repository Favorite
    adjusment

--- src/main/java/com/atomic/getTentor/repository/FavoriteRepository.java
+++ src/main/java/com/atomic/getTentor/repository/FavoriteRepository.java
@@ -1,11 +1,13 @@
-package com.example.favorite.repository;
+package com.atomic.getTentor.repository;
 
-import com.example.favorite.model.Favorite;
-import com.example.favorite.model.FavoriteId;
+import com.atomic.getTentor.model.Favorite;
+import com.atomic.getTentor.model.FavoriteId;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
 public interface FavoriteRepository extends JpaRepository<Favorite, FavoriteId> {
-    List<Favorite> findByMenteeId(Integer menteeId);
+    List<Favorite> findByMentee_Id(Integer menteeId);
+    boolean existsByMentee_IdAndTentor_Id(Integer menteeId, Integer tentorId);
+    void deleteByMentee_IdAndTentor_Id(Integer menteeId, Integer tentorId);
 }
