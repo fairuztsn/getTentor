5c43ef97f843bfd47003d53798a458e68fd6c4a0
fae <101633871+fairuztsn@users.noreply.github.com>
2025-05-21 20:08:48 +0700

    Update ReviewRepository.java
    - Change param type from Long to Integer
    - add findByMenteeAndTentor

--- src/main/java/com/atomic/getTentor/repository/ReviewRepository.java
+++ src/main/java/com/atomic/getTentor/repository/ReviewRepository.java
@@ -1,11 +1,15 @@
 package com.atomic.getTentor.repository;
+import com.atomic.getTentor.model.Mentee;
+import com.atomic.getTentor.model.Tentor;
 import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
+import java.util.Optional;
+
 import com.atomic.getTentor.model.Review;
 
 public interface ReviewRepository extends JpaRepository<Review, Long> {
-    
-    List<Review> findByMenteeId(Long menteeId);
-    List<Review> findByTentorId(Long tentorId);
+    List<Review> findByMenteeId(Integer menteeId);
+    List<Review> findByTentorId(Integer tentorId);
+    Optional<Review> findByMenteeAndTentor(Mentee mentee, Tentor tentor);
 }
 
