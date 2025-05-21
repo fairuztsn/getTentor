21afebaf5e293d4d2561c7addee3f49efa752714
Gkyoto <dickarksr0725@gmail.com>
2025-05-21 18:57:12 +0700

    adjustment Favorite
    adjustment

--- src/main/java/com/atomic/getTentor/model/Favorite.java
+++ src/main/java/com/atomic/getTentor/model/Favorite.java
@@ -1,10 +1,10 @@
-package com.example.favorite.model;
+package com.atomic.getTentor.model;
 
 import jakarta.persistence.*;
-import java.io.Serializable;
 
 @Entity
 @IdClass(FavoriteId.class)
+@Table(name = "favorite")
 public class Favorite {
     @Id
     @ManyToOne
@@ -16,5 +16,22 @@ public class Favorite {
     @JoinColumn(name = "tentor_id")
     private Tentor tentor;
 
+    public Favorite() {}
+
     // Getters and setters
+    public Mentee getMentee() {
+        return mentee;
+    }
+
+    public void setMentee(Mentee mentee) {
+        this.mentee = mentee;
+    }
+
+    public Tentor getTentor() {
+        return tentor;
+    }
+
+    public void setTentor(Tentor tentor) {
+        this.tentor = tentor;
+    }
 }
