92506da1c85680b72b7e5e917fda1bb05d5f97ed
fae <101633871+fairuztsn@users.noreply.github.com>
2025-05-29 08:30:29 +0700

    feat: add ratingCount and averageRating on tentor DTO

--- src/main/java/com/atomic/getTentor/dto/TentorDTO.java
+++ src/main/java/com/atomic/getTentor/dto/TentorDTO.java
@@ -23,6 +23,8 @@ public class TentorDTO {
     private List<MataKuliahDTO> listMataKuliah;
     private String fotoUrl;
     private String noTelp;
+    private Double averageRating;
+    private Integer ratingCount;
 
     public TentorDTO() {}
 
@@ -52,6 +54,8 @@ public class TentorDTO {
         this.listMataKuliah = tentor.getListMataKuliah() == null ? new ArrayList<>() : tentor.getListMataKuliah().stream()
                 .map(MataKuliahDTO::new)
                 .toList();
+        this.averageRating = tentor.getAverageRating();
+        this.ratingCount = this.listReview.size();
     }
 
     public Integer getId() { return id; }
@@ -67,6 +71,8 @@ public class TentorDTO {
     public List<MataKuliahDTO> getListMataKuliah() {
         return listMataKuliah;
     }
+    public Double getAverageRating() { return averageRating; }
+    public Integer getRatingCount() { return ratingCount; }
 
     @JsonIgnore
     public String getPassword() {
@@ -85,6 +91,8 @@ public class TentorDTO {
     public void setListMataKuliah(List<MataKuliahDTO> listMataKuliah) {
         this.listMataKuliah = listMataKuliah;
     }
+    public void setAverageRating(Double averageRating) { this.averageRating = averageRating; }
+    public void setRatingCount(Integer ratingCount) { this.ratingCount = ratingCount; }
 
     public void setEmail(String email) {
         if (email == null || email.trim().isEmpty()) {
