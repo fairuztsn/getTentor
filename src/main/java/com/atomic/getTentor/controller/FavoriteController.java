604c5dd1021f82cf98d9e55865076160a1e3b90e
fae <101633871+fairuztsn@users.noreply.github.com>
2025-05-29 17:56:12 +0700

    Adjust favorite controller

--- src/main/java/com/atomic/getTentor/controller/FavoriteController.java
+++ src/main/java/com/atomic/getTentor/controller/FavoriteController.java
@@ -1,5 +1,6 @@
 package com.atomic.getTentor.controller;
 
+import com.atomic.getTentor.dto.TentorDTO;
 import com.atomic.getTentor.model.Favorite;
 import com.atomic.getTentor.model.Mentee;
 import com.atomic.getTentor.model.Tentor;
@@ -8,6 +9,7 @@ import com.atomic.getTentor.repository.MenteeRepository;
 import com.atomic.getTentor.repository.TentorRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
@@ -27,19 +29,17 @@ public class FavoriteController {
     @Autowired
     private TentorRepository tentorRepository;
 
-    @GetMapping
-    public List<Favorite> getAll() {
-        return favoriteRepository.findAll();
-    }
-
     @GetMapping("/{menteeId}")
-    public List<Favorite> getByMenteeId(@PathVariable Integer menteeId) {
-        return favoriteRepository.findByMentee_Id(menteeId);
+    public List<TentorDTO> getFavoritesByMentee(@PathVariable Integer menteeId) {
+        List<Favorite> favorites = favoriteRepository.findByMentee_Id(menteeId);
+        return favorites.stream()
+                .map(fav -> new TentorDTO(fav.getTentor()))
+                .toList();
     }
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    public Favorite addFavorite(@RequestParam Integer menteeId, @RequestParam Integer tentorId) {
+    public void addFavorite(@RequestParam Integer menteeId, @RequestParam Integer tentorId) {
         // Cek apakah kombinasi sudah ada
         boolean exists = favoriteRepository.existsByMentee_IdAndTentor_Id(menteeId, tentorId);
         if (exists) {
@@ -56,7 +56,7 @@ public class FavoriteController {
         Favorite favorite = new Favorite();
         favorite.setMentee(mentee);
         favorite.setTentor(tentor);
-        return favoriteRepository.save(favorite);
+        favoriteRepository.save(favorite);
     }
 
     @Transactional
